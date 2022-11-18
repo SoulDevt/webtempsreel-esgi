09fcc416786f304a4dbc565977ff4cd1ad73be08
Clarence Potel <potel.clarence@gmail.com>
2022-11-18 15:45:48 +0100

    add env var

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8000,
-    host: "0.0.0.0"
-  }
-})
-
-
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      port: env.PORT_CLIENT || 8000,
+      host: "0.0.0.0",
+    },
+    define: {
+      "process.env": env,
+    }
+  };
+});
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const server = require('./server');
-const port = process.env.PORT || 9000
+require("dotenv").config();
+const server = require("./server");
+const port = process.env.PORT_SERVER || 9000;
 
 server.listen(port, () => {
   console.log("Server is listening on port " + port);
