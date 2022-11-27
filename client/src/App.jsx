b3f3e2c32560249485e46f3d340dce62686b94d5
Clarence Potel <potel.clarence@gmail.com>
2022-11-27 16:29:51 +0100

    update socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom'
-import { Home, Error } from './pages';
+import { useEffect, useMemo } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, Error, Login } from './pages';
 import { io } from 'socket.io-client';
 
-const socket = io(`http://${process.env.HOST||'localhost'}:${process.env.PORT_SERVER || 9000}`);
-
 const App = () => {
+  const socket = useMemo(() => io(`http://${process.env.HOST || 'localhost'}:${process.env.PORT_SERVER || 9000}`), []);
+
   useEffect(() => {
-    socket.on("connect", () => {
+    socket.on('connect', () => {
       console.log('client conectado');
     });
+    return () => {
+      socket.disconnect();
+    };
   }, [socket]);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
       <Route path="*" element={<Error />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
