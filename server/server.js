965c253b1d97c9fcc6c2b653aa8aae6dc8b5836c
Clarence Potel <potel.clarence@gmail.com>
2022-11-27 16:20:24 +0100

    fix server disconnection

--- server/server.js
+++ server/server.js
@@ -3,26 +3,41 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+// const EventEmitter = require("events");
 const mainRouter = require("./routes");
 
+// const notificationEvent = new EventEmitter();
 const app = express();
 app.use(cors());
 app.use(mainRouter);
 
+// app.use(express.static("./notification.js"));
+
+// app.get("/api/notification", (request, response) => {
+//   response.setHeader("Content-Type", "text/event-stream");
+//   response.setHeader("Connection", "keep-alive");
+//   response.setHeader("Cache-Control", "no-cache");
+
+//   notificationEvent.on("update", (users) => {
+//     response.write(`event: update\ndata: ${JSON.stringify(users)}\n\n`);
+//   });
+// });
 const server = http.createServer(app);
 
 const io = new Server(server, {
-	cors: {
-		origin: `http://${process.env.HOST || 'localhost'}:${process.env.PORT_CLIENT||8000}`,
-		methods: ["GET", "POST"],
-	},
+  cors: {
+    origin: `http://${process.env.HOST || "localhost"}:${
+      process.env.PORT_CLIENT || 8000
+    }`,
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-	console.log(`User connected ${socket.id}`);
-	io.on("disconnect", () => {
-		console.log("Client disconnected");
-	});
+  console.log(`User connected ${socket.id}`);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 // app.use((req, res, next) => {
