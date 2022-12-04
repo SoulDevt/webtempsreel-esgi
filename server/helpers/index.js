ba51d3f37336270ad8c227a2178cd8ce3bab28dc
Clarence Potel <potel.clarence@gmail.com>
2022-12-05 00:05:15 +0100

    delete files

--- server/helpers/index.js
+++
@@ -1,1 +0,0 @@
-exports.SseHelpers = require('./sse');
--- server/routes/sse.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { NotificationController } = require("../controller");
-
-const router = new Router();
-
-router.get("/notification", NotificationController.getNotification);
-router.post("/notification", NotificationController.postNotification);
-
-module.exports = router;
