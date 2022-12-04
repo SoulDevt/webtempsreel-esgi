a7bdd2fadb0d858e0a4db6f2cd7717fd6fadbd1f
Clarence Potel <potel.clarence@gmail.com>
2022-12-05 00:08:06 +0100

    handle notification sse client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,66 @@
-import { useEffect, useMemo, lazy, Suspense } from 'react';
+import { useEffect, useState, useMemo, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Nav } from './components';
 import { io } from 'socket.io-client';
 import { serverUrl } from './enums';
-import { Loader } from './components';
+import { Loader, Toast } from './components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const Home = lazy(() => import('./pages/Home'));
 const Error = lazy(() => import('./pages/Error'));
 const Login = lazy(() => import('./pages/Login'));
-const Test = lazy(() => import('./pages/Test'));
+const Notification = lazy(() => import('./pages/Notification'));
 
 const App = () => {
   const socket = useMemo(() => io(serverUrl), []);
+  const [listenning, setListenning] = useState(false);
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('client conectado');
-    });
+    const source = new EventSource(`${serverUrl}/stream`, { withCredentials: true });
+    if (!listenning) {
+      socket.on('connect', () => {
+        console.log('client conectado');
+      });
+
+      source.addEventListener('open', () => {
+        console.log('SSE opened!');
+      });
+
+      source.addEventListener('notification', async (e) => {
+        const { data } = JSON.parse(e.data);
+        if (data.status === 'created') {
+          toast(<Toast titre={data?.titre} message={data?.message} />, {
+            theme: 'dark'
+          });
+        }
+      });
+
+      source.addEventListener('error', (error) => {
+        // Prints the information about an error
+        console.log(error);
+      });
+
+      source.addEventListener('close', () => {
+        console.log('SSE closed!');
+      });
+
+    }
+
+    setListenning(true);
     return () => {
       socket.disconnect();
+      source.removeEventListener('notification', () => {});
+      source.close();
     };
   }, [socket]);
   return (
     <Suspense fallback={<Loader />}>
       <Nav />
+      <ToastContainer />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/notification" element={<Notification />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
---
+++ client/src/pages/Notification.jsx
@@ -0,0 +1,56 @@
+import { useCallback, useState } from 'react';
+import { serverUrl } from '../enums';
+import { toast } from 'react-toastify';
+import { StrHelper } from '../helpers';
+
+const Notification = () => {
+  const [formData, setFormData] = useState({ titre: '', message: '' });
+  const [error, setError] = useState(null);
+
+  const handleChange = useCallback(
+    (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setFormData({ ...formData, [name]: value });
+      setError(null);
+    },
+    [formData]
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { titre, message } = formData;
+    if (!titre.trim() || !message.trim()) return setError('Veuillez remplir tous les champs');
+    try {
+      const res = await fetch(`${serverUrl}/notification`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ titre: titre.trim().capitalize(), message: message.trim(), status: 'created' })
+      });
+      setFormData({ titre: '', message: '' });
+    } catch (error) {
+      console.log(error);
+      toast.error('Une erreur est survenue');
+    }
+  };
+
+  return (
+    <>
+      <div className="Notification container mx-auto px-4 flex flex-col justify-center items-center">
+        <h1>Envoyer une notification</h1>
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <label htmlFor="titre">Titre</label>
+          <input id="titre" name="titre" type="text" value={formData.titre} onInput={handleChange} />
+          <label htmlFor="message">Message</label>
+          <input id="message" name="message" type="text" value={formData.message} onInput={handleChange} />
+          <p>{error}</p>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Notification;
