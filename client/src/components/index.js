f37587014a007ba6ae635e5894f9c670e81ea88e
Clarence Potel <potel.clarence@gmail.com>
2022-12-15 21:01:31 +0100

    check auth component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Nav from './Nav';
 import Loader from './Loader';
 import Toast from './Toast';
+import RequireAuth from './RequiredAuth';
 
-export { Nav, Loader, Toast };
+export { Nav, Loader, Toast, RequireAuth };
