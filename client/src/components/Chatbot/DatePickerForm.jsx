35bdb59f40e770528ae2f3ce0b5e51fa700ea667
Clarence Potel <potel.clarence@gmail.com>
2023-01-05 16:19:12 +0100

    add cancel btn

--- client/src/components/Chatbot/DatePickerForm.jsx
+++ client/src/components/Chatbot/DatePickerForm.jsx
@@ -1,38 +1,41 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DatePickerForm = ({ choosenEvent, handleSubmit, handleDatePicker, handleTitle }) => {
+const DatePickerForm = ({ choosenEvent, handleSubmit, handleDatePicker, handleTitle, cancel }) => {
   return (
-  <form className="flex items-center w-full">
-    <input
-      type="text"
-      placeholder="Titre de la réservation"
-      className="border border-slate-800 rounded-md p-2 mr-3"
-      name="title"
-      value={choosenEvent.title}
-      onInput={handleTitle}
-    />
-    <DatePicker
-      name="date"
-      selected={choosenEvent.date}
-      onChange={handleDatePicker}
-      value={choosenEvent.date}
-      minDate={new Date()}
-      dateFormat="dd/MM/yyyy"
-      className="border border-slate-800 rounded-md p-2 z-10 relative"
-      showTimeSelect
-      timeFormat="HH:mm"
-      timeIntervals={60}
-      minTime={new Date().setHours(9, 0, 0)}
-      maxTime={new Date().setHours(17, 0, 0)}
-      timeCaption="Heure"
-      placeholderText="Choisissez une date"
-    />
-    <button type="submit" className=" dark:text-gray-200 hover:text-gray-400 bg-slate-800" onClick={handleSubmit}>
-      Valider
-    </button>
+    <form className="flex items-center w-full">
+      <input
+        type="text"
+        placeholder="Titre de la réservation"
+        className="border border-slate-800 rounded-md p-2 mr-3"
+        name="title"
+        value={choosenEvent.title}
+        onInput={handleTitle}
+      />
+      <DatePicker
+        name="date"
+        selected={choosenEvent.date}
+        onChange={handleDatePicker}
+        value={choosenEvent.date}
+        minDate={new Date()}
+        dateFormat="dd/MM/yyyy"
+        className="border border-slate-800 rounded-md p-2 z-10 relative"
+        showTimeSelect
+        timeFormat="HH:mm"
+        timeIntervals={60}
+        minTime={new Date().setHours(9, 0, 0)}
+        maxTime={new Date().setHours(17, 0, 0)}
+        timeCaption="Heure"
+        placeholderText="Choisissez une date"
+      />
+      <button type="submit" className="dark:text-gray-200 hover:text-gray-400 bg-slate-800 mr-2" onClick={handleSubmit}>
+        Valider
+      </button>
+      <button className="dark:text-gray-200 hover:text-gray-400 bg-slate-800" onClick={cancel}>
+        Annuler
+      </button>
     </form>
-  )
+  );
 };
 
 export default DatePickerForm;
